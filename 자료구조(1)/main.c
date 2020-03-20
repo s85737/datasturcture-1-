d7f952dabfaaeefc3f907676d52ebe84035d4016
jehyeon <s85737@gmail.com>
2020-03-20 10:52:35 +0900

    Initial Commit

---
+++ 자료구조(1)/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  자료구조(1)
+//
+//  Created by 최제현 on 2020/03/20.
+//  Copyright © 2020 5468107최제현. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
