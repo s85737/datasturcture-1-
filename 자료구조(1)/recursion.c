5421f9357e87bc8097239c5d502e76f3a2a6ff20
jehyeon <s85737@gmail.com>
2020-03-23 19:44:49 +0900

    3/23 과제

--- 자료구조(1)/recursion.c
+++ 자료구조(1)/recursion.c
@@ -9,48 +9,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// iteration factorial
+// iteration 방식 factorial
 int iFactorial(int paramNum){
-    int i =1;
     int result=1;
-    for (i = 1; i <= paramNum; i++){
+    
+    for (int i = 1; i <= paramNum; i++){
         result = result * i;
     };
     
     return result;
 }
 
-// recursion factorial
-
+// recursion 방식 factorial
 int rFactorial(int paramNum){
-    if(paramNum <= 1) return 1;
+    if(paramNum <= 1) return 1;  // n이 1 이하로 갈 경우 1을 반환하고 재귀호출 종료
     else return (paramNum * rFactorial(paramNum-1));
 }
 
-int main() {
+int main(void) {
     clock_t startIter, stopIter, startRecu, stopRecu;
     double durationIter, durationRecu;
     double result = 0;
-    int argNum;
-    
-    printf("몇 펙토리얼?\n");
-    scanf("%d", &argNum);
-    
-    //interation 방식
-    startIter = clock();  // 시간 측정 시작
-    result = iFactorial(argNum);
-    stopIter = clock(); // 시간 측정 중지
-    durationIter = (double)(stopIter - startIter) / CLOCKS_PER_SEC;
-    printf("interation 방식의 수행시간은 %lf초 입니다.\n", durationIter);
-    printf("답 : %lf\n", result);
-    
-    //recursion 방식
-    startRecu = clock();  // 시간 측정 시작
-    result = rFactorial(argNum);
-    stopRecu = clock(); // 시간 측정 중지
-    durationRecu = (double)(stopRecu - startRecu) / CLOCKS_PER_SEC;
-    printf("recursion 방식의 수행시간은 %lf초 입니다.\n", durationRecu);
-    printf("답 : %lf\n", result);
-    
-    return 0;
+    int argNum=0;
+    while(1){
+        printf("몇 factorial 을 계산할까요? 0을 입력하면 종료합니다.\n");
+        scanf("%d", &argNum);
+        if(argNum>0){
+            //interation 방식 시간측정
+            startIter = clock();  // 시간 측정 시작
+            result = iFactorial(argNum);
+            stopIter = clock(); // 시간 측정 중지
+            // 측정한 시간을 초 단위 변환
+            durationIter = (double)(stopIter - startIter) / CLOCKS_PER_SEC;
+            printf("%d!의 interation 방식의 수행시간은 %lf초 입니다.\n",argNum, durationIter);
+            printf("답 : %.0lf\n", result);
+            
+            //recursion 방식 시간측정
+            startRecu = clock();  // 시간 측정 시작
+            result = rFactorial(argNum);
+            stopRecu = clock(); // 시간 측정 중지
+            durationRecu = (double)(stopRecu - startRecu) / CLOCKS_PER_SEC;
+            printf("%d!의 recursion 방식의 수행시간은 %lf초 입니다.\n",argNum ,durationRecu);
+            printf("답 : %.0lf\n", result);
+            continue;
+            }
+        else printf("프로그램을 종료합니다.\n");
+        return 0;
+    }
 }
--- 자료구조(1)/recursionOverflow.c
+++ 자료구조(1)/recursionOverflow.c
@@ -7,3 +7,34 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+// 함수 호출 count
+int count = 0;
+
+// recursion 방식 factorial
+
+int recursion(int paramNum){
+    count++; // 함수가 한번 호출 될 때마다 1번 count
+    if(paramNum <= 1) return 1; // n이 1 이하로 갈 경우 1을 반환하고 재귀호출 종료
+    else{
+        printf("%d회 호출\n", count);
+        return (paramNum * recursion(paramNum-1));
+    }
+}
+
+int compare(void) {
+    
+    int argNum;
+    double result=0;
+    
+    printf("몇 factorial 을 계산할까요?\n");
+    scanf("%d", &argNum);
+    
+    result = recursion(argNum);
+    
+    printf("결과%.0lf\n", result); //함수가 게산 완료했을 때 결과출력
+    return 0;
+}
+
+
